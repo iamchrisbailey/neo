0910969a1725ae94f65cbe31425a547a598752a8
iamchrisbailey <iamchrisbailey@gmail.com>
2013-08-10 10:13:10 -0400

    updated Droid font references and added module enable script.

---
+++ scripts/enable_modules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Written by Chris Bailey.
+#This script will enable modules needed for the new england outerwear website.
+drush en -y fontyourface
+drush en -y fontyourface_ui
+drush en -y google_fonts_api
