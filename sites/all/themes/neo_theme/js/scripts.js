15a8e2da63dd5b6813b1271754da865c275e4d98
iamchrisbailey <iamchrisbailey@gmail.com>
2013-10-22 13:56:15 -0400

    added dropshadow scroll affect to top bar

--- sites/all/themes/neo_theme/js/scripts.js
+++ sites/all/themes/neo_theme/js/scripts.js
@@ -12,3 +12,17 @@ $(document).ready(function() {
 			});
 		});
 }(jQuery));
+
+(function($) {
+$(document).ready(function() {		
+			$(window).scroll(function(){
+				var scrollTop = $(window).scrollTop();
+				if(scrollTop >= 1) {
+					$('.zone-user-wrapper').stop().css('box-shadow', '0 0 10px rgba(0, 0, 0, 0.2)');
+				}
+				else   {
+					$('.zone-user-wrapper').stop().css('box-shadow', '0 0 0 rgba(0, 0, 0, 0.5)');
+				}
+			});
+		});
+}(jQuery));
